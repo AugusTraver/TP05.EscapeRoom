fa6f4aa2d9cc83b6665b804faa2f72e4e0c9dbc5
AUGUSTO AMADEO TRAVERSO <49315675@ort>
2025-06-13 15:59:01 -0300

    En la sala 4

--- Models/EscapeRoom.cs
+++ Models/EscapeRoom.cs
@@ -18,12 +18,12 @@ public class Escaperoom
         respuestas = new Dictionary<int, string>();
         pistas.Add(1, "Fijate en la grulla");
         pistas.Add(2, "Mira el nombre del barco");
-        pistas.Add(3, "Toca la lampara");
+        pistas.Add(3, "El video esta en loop, no toques para asi no pausarlo, apagado vale 0, titleo 1 y prendido 2, pone el codigo y pasa a la siguiente sala");
         pistas.Add(4, "Fijate en el locer 7 y en el video cuando dice aguas...");
         pistas.Add(5, "Para sacar la calle mira la carta y para sacar el numero mira el plano");
         respuestas.Add(1, "ORIGAMI");
         respuestas.Add(2, "CHRISTINAROSE");
-        respuestas.Add(3, "210110");
+        respuestas.Add(3, "21210");
         respuestas.Add(4, "AGUAS TERMALES");
         respuestas.Add(5, "FOX RIVER ROAD 42");
     }
