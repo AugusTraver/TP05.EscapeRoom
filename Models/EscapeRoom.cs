7eb9ba7137c58320d193e75ae683355f2dd29a16
unknown <augustraver@gmail.com>
2025-06-13 19:43:45 -0300

    css mejorado

--- Models/EscapeRoom.cs
+++ Models/EscapeRoom.cs
@@ -18,7 +18,7 @@ public class Escaperoom
         respuestas = new Dictionary<int, string>();
         pistas.Add(1, "Fijate en la grulla");
         pistas.Add(2, "Mira el nombre del barco");
-        pistas.Add(3, "El video esta en loop, no toques para asi no pausarlo, apagado vale 0, titleo 1 y prendido 2, pone el codigo y pasa a la siguiente sala");
+        pistas.Add(3, "El video esta en loop, no toques para asi no pausarlo, apagado vale 0, titleo 1 y prendido 2, pone el codigo y pasa a la siguiente sala, termina cuando esta apagada");
         pistas.Add(4, "Fijate en el locer 7 y en el video cuando dice aguas...");
         pistas.Add(5, "Para sacar la calle mira la carta y para sacar el numero mira el plano");
         respuestas.Add(1, "ORIGAMI");
