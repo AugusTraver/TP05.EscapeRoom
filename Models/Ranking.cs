77a6a50be85ff99ce9e7c9140066e4b355ddaa2d
AUGUSTO AMADEO TRAVERSO <49315675@ort>
2025-06-26 09:22:18 -0300

    Falta terminar Ranking

--- Models/Ranking.cs
+++ Models/Ranking.cs
@@ -10,6 +10,7 @@ public static void AgregarUsuario(string NomUsu, TimeSpan Tiempo)
         DicRanking.Add(NomUsu, Tiempo);
 }public static Dictionary<string, TimeSpan> ObtenerRankingOrdenadoDic()
 {
+        Console.WriteLine($"Obteniendo ranking: {DicRanking.Count} jugadores");
     return DicRanking
      .OrderByDescending(pair => pair.Value)
      .ToDictionary(pair => pair.Key, pair => pair.Value);
